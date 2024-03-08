65fac3eb520b5d5c69248ee1bbf6bd90717a0cad
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-09 01:40:32 +1100

    scoreKeeper created

---
+++ scoreKeeping.js
@@ -0,0 +1,46 @@
+const reset = document.querySelector('#reset')
+const maxScore = document.querySelector('#maxScore')
+let gameIsOver = false
+
+
+const p1All = {
+    score: document.querySelector('#p1Score'),
+    add: document.querySelector('#p1Add'),
+
+}
+
+const p2All = {
+    score: document.querySelector('#p2Score'),
+    add: document.querySelector('#p2Add')
+
+}
+
+function updateScore(player, opponent) {
+    if (!gameIsOver) {
+        let newValue = parseInt(player.score.innerText) + 1
+        player.score.innerText = newValue
+        if (parseInt(player.score.innerText) === parseInt(maxScore.value)) {
+            player.score.style.color = 'green';
+            opponent.score.style.color = 'red';
+            gameIsOver = true
+        }
+    }
+
+}
+
+p1All.add.addEventListener('click', function () {
+    updateScore(p1All, p2All)
+})
+p2All.add.addEventListener('click', function () {
+    updateScore(p2All, p1All)
+})
+
+function resetScore() {
+    gameIsOver = false;
+    p1All.score.innerText = 0;
+    p1All.score.style.color = "";
+    p2All.score.innerText = 0;
+    p2All.score.style.color = "";
+}
+reset.addEventListener('click', resetScore)
+maxScore.addEventListener('change', resetScore)
